8401bf9021f992fe3e84f2bd6ada09f46cdb37cd
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-07 21:48:23 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Hino do Corinthians");
+Console.ResetColor();
+
+Console.WriteLine(@"Salve o Corinthians O campeão dos campeões 
+Eternamente Dentro dos nossos corações
+
+Salve o Corinthians
+De tradições e glórias mil
+Tu és orgulho
+Dos desportistas do Brasil
+
+Teu passado é uma bandeira
+Teu presente é uma lição
+Figuras entre os primeiros
+Do nosso esporte bretão
+
+Corinthians grande
+Sempre altaneiro
+És do Brasil
+O clube mais brasileiro.");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"Salve o Corinthians
+O campeão dos campeões
+Eternamente
+Dentro dos nossos corações");
+Console.ResetColor();
+
+Console.WriteLine(@"Salve o Corinthians
+De tradições e glórias mil
+Tu és orgulho
+Dos desportistas do Brasil.");
